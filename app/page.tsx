5b6a18731d758b9240c7d12b6b7ea6d34e1d86a7
dantrua001 <93893049+dantrua001@users.noreply.github.com>
2025-02-26 20:57:32 -0500

    Bootstrap additions
    Added outline button and stack layout to static page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 'use client'
-import Image from "next/image";
 
 import React, { useState } from "react";
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const People = [
     { name: "Sue", favoriteFood: "Pizza", favoriteColor: "Blue" },
@@ -11,21 +13,21 @@ const People = [
 
 const Card = ({ person }) => {
     const [likes, setLikes] = useState(0);
-
+    
     return (
-        <div style={{ border: "1px solid #ccc", padding: "10px", margin: "10px", borderRadius: "8px" }}>
+        <Stack gap={2} className="border p-2 m-2 rounded">
             <h2>{person.name}</h2>
             <p>Favorite Food: {person.favoriteFood}</p>
             <p>Favorite Color: {person.favoriteColor}</p>
-            <button onClick={() => setLikes(likes + 1)}>Like ({likes})</button>
-        </div>
+            <Button  variant="outline-primary" onClick={() => setLikes(likes + 1)}>Like ({likes})</Button>
+        </Stack>
     );
 };
 
 const App = () => {
     return (
         <div>
-            <h1>People List</h1>
+            <h1>My Classmates</h1>
             {People.map((person, index) => (
                 <Card key={index} person={person} />
             ))}
